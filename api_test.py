26b5e9cb9fb0f64e248e0b820981c7cb11d703f5
mail@harrysar.dev <mail@harrysar.dev>
2022-01-25 14:29:44 +0000

    Api testing

---
+++ api_test.py
@@ -0,0 +1,13 @@
+import requests,json
+
+def get_flight_data(api_key):
+    main_url="https://airlabs.co/api/v9/flights?api_key="+api_key
+
+
+
+
+
+if __name__=="__main__":
+    flight_api_key = ""
+    get_flight_data(flight_api_key)
+
