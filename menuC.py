45b96219ac4b462b02ca681eb1c0777435287768
mail@harrysar.dev <mail@harrysar.dev>
2022-02-15 13:45:44 +0000

    Added Menu class layout (Aisling to complete)

---
+++ menuC.py
@@ -0,0 +1,26 @@
+class menuObj():
+
+    def __init__(self):
+        self.choice=None
+
+
+    def displayMenu(self):
+        print("----------------------------\n"
+              "Flight & Tourism Data Menu\n"
+              "----------------------------")
+        # Aisling to complete menu class from here
+        # use if "____" in [""] for checks
+        # run self. instance of functions within class when user choice is matched
+
+
+
+    def checkData(self,dataToSearch):
+        quit(0)
+        # so dataToSearch is the data you are looking for within the csv or internal JSON data files
+        # search this for the required data
+
+
+
+    def visualiseData(self):
+        quit(0)
+        # placeholder function for all visualisations later on.
