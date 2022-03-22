4224a0fec7df4e32d0f3d667a9d282d0492c84ec
CallumT654 <callum-taylor65@outlook.com>
2022-03-22 14:13:50 +0000

    Added CSV to ny branch

--- test_file.py
+++ test_file.py
@@ -1,6 +1,17 @@
 import csv
+from gettext import install
+
+import matplotlib
 
 with open('unwto-inbound-arrivals-data.csv', 'r') as f:
     csv_reader = csv.reader(f)
     header_row = next(csv_reader)
 print(header_row)
+
+import matplotlib.pyplot as plt
+
+plt.plot(xAxis,yAxis)
+plt.title('title name')
+plt.xlabel('xAxis name')
+plt.ylabel('yAxis name')
+plt.show()
