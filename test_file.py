e8a7a57c655cfdbdb1c407ded86a8c1d8d5a6e21
ashmoran2002@gmail.com <ashmoran2002@gmail.com>
2022-03-29 01:00:37 +0100

    wee updates

--- test_file.py
+++ test_file.py
@@ -1,11 +1,18 @@
-def test():
-    print("Hello im Aisling")
+import matplotlib.pyplot as plt
+import csv
 
+destinations = [1, 2, 3, 4, 5]
+incomingFlights = [1, 4, 9, 16, 25]
+touristHistory = [5, 10, 15, 20, 25]
 
+fig, axs = plt.subplots(1, 2)
 
+axs.plot(destinations, incomingFlights, touristHistory)
 
+axs[0].setTitle("Busiest Airports")
+axs[1].setTitle("Popular Tourist Destinations")
 
+axs[0].bar(destinations, incomingFlights)
+axs[1].line(destinations, touristHistory)
 
-
-if __name__ == "__main__":
-    test()
+plt.show()
