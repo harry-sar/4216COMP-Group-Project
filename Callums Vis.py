3c31eab1586ada5c4c311c065fef782eecb964ca
CallumT654 <callum-taylor65@outlook.com>
2022-04-08 18:49:50 +0100

    added legand

--- Callums Vis.py
+++ Callums Vis.py
@@ -1,6 +1,8 @@
+from turtle import left, right
+import numpy as np
 import matplotlib.pyplot as plt
 
-years = [1995, 1999, 2003, 2007, 2011, 2015, 2019, ]
+years = [1995, 1999, 2003, 2007, 2011, 2015, 2019]
 SriLankatourists = [414, 465, 583, 592, 976, 1993, 2027]
 UKtourists = [23537, 25394, 24715, 32778, 31886, 36792, 40857]
 Bahamastourists = [3239, 3648, 4594, 4601, 5588, 6112, 7250]
@@ -17,5 +19,8 @@ ax.plot(years, Ukrainetourists, 'gD:')
 
 ax.set_xlabel("Years", fontsize=12)
 ax.set_ylabel("Number of Tourists (thousands)", fontsize=12)
+
+plt.gca().legend(('Sri Lanka','UK', 'Bahamas', 'Canada', 'Ukraine',)) 
+plt.legend(bbox_to_anchor=(1.05, 1.0, 0.3, 0.2), loc='upper left')
 
 plt.show()
